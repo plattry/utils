00d27734f1ced77660f51c6e18dc94e430108f42
Andrew Lee <sixser@foxmail.com>
2021-10-21 22:29:40 +0800

    Optimize Debug::handleThrow

--- src/Debug.php
+++ src/Debug.php
@@ -34,29 +34,10 @@ class Debug
     public static function handleThrow(Throwable $t): void
     {
         $content = sprintf(
-            "\r\nAn %s thrown with %s(%d) in %s(%d) at %s\r\n",
-            $t::class, $t->getMessage(), $t->getCode(), $t->getFile(), $t->getLine(), static::getCurrTime()
+            "\r\n%s\r\nTHROWN: %s\r\nCODE: %d\r\nPOSITION: %s(%d)\r\nMESSAGE: %s\r\n%s\r\n",
+            static::getCurrTime(), get_class($t), $t->getCode(), $t->getFile(),
+            $t->getLine(), $t->getMessage(), $t->getTraceAsString()
         );
-
-        $flags = JSON_FORCE_OBJECT | JSON_UNESCAPED_UNICODE;
-
-        foreach ($t->getTrace() as $index => $trace) {
-            if (isset($trace['file']))
-                $content .= sprintf(
-                    "from %d: %s(%d) on %s(%s)\r\n",
-                    $index, $trace['file'], $trace['line'], $trace['function'], json_encode($trace['args'], $flags)
-                );
-            elseif (isset($trace['class']))
-                $content .= sprintf(
-                    "from %d: %s on %s(%s)\r\n",
-                    $index, $trace['class'], $trace['function'], json_encode($trace['args'], $flags)
-                );
-            else
-                $content .= sprintf(
-                    "from %d: on %s(%s)\r\n",
-                    $index, $trace['function'], json_encode($trace['args'], $flags)
-                );
-        }
 
         fwrite(STDERR, $content);
     }
