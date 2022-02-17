a0b46431aae0e86725327566c5435a360ab2241a
Andrew Lee <sixser@foxmail.com>
2022-02-17 21:47:50 +0800

    Fixed Filesystem::findClass(skip ::class constant and anonymous classes)

--- src/Filesystem.php
+++ src/Filesystem.php
@@ -44,33 +44,60 @@ class Filesystem
      */
     public static function findClass(string $filename): string|null
     {
-        !file_exists($filename) &&
-        throw new \InvalidArgumentException("$filename is not an exist file.");
-
-        $content = file_get_contents($filename);
-        $content === false &&
-        throw new \RuntimeException("An error occur while getting $filename content.");
-
-        $tokens = token_get_all($content);
-        (count($tokens) < 1 || T_INLINE_HTML === $tokens[0][0]) &&
-        throw new \InvalidArgumentException("$filename is not a file that contains valid PHP code.");
+        $tokens = \token_get_all(\file_get_contents($filename) ?: '');
+        (\count($tokens) === 1 || $tokens[0][0] === \T_INLINE_HTML) &&
+        throw new \InvalidArgumentException(sprintf('The file "%s" does not contain PHP code', $filename));
 
         $namespace = $class = false;
-        foreach ($tokens as $token) {
-            if (!is_array($token) || !isset($token[1]))
+        for ($i = 0; isset($tokens[$i]); ++$i) {
+            $token = $tokens[$i];
+            if (!isset($token[1])) {
                 continue;
+            }
 
-            // found flag, and will be getting namespace or class name
-            if (true === $namespace && T_NAME_QUALIFIED === $token[0])
-                $namespace = $token[1];
-            elseif (true === $class && T_STRING === $token[0])
-                return "$namespace\\$token[1]";
+            switch ($token[0]) {
+                case \T_NAMESPACE:
+                    $namespace = true;
 
-            // check namespace or class name flag
-            if (T_NAMESPACE === $token[0])
-                $namespace = true;
-            elseif (T_CLASS === $token[0])
-                $class = true;
+                    break;
+                case \T_CLASS:
+                    // Skip ::class constant and anonymous classes
+                    $skipClassToken = false;
+                    for ($j = $i - 1; $j > 0; --$j) {
+                        if (!isset($tokens[$j][1])) {
+                            if ('(' === $tokens[$j] || ',' === $tokens[$j]) {
+                                $skipClassToken = true;
+                            }
+                            break;
+                        }
+
+                        if (\T_DOUBLE_COLON === $tokens[$j][0] || \T_NEW === $tokens[$j][0]) {
+                            $skipClassToken = true;
+                            break;
+                        } elseif (!\in_array($tokens[$j][0], [\T_WHITESPACE, \T_DOC_COMMENT, \T_COMMENT])) {
+                            break;
+                        }
+                    }
+
+                    if (!$skipClassToken) {
+                        $class = true;
+                    }
+
+                    break;
+                case \T_NS_SEPARATOR:
+                case \T_NAME_QUALIFIED:
+                case \T_STRING:
+                    if ($namespace === true) {
+                        $namespace = $token[1];
+                        while (isset($tokens[++$i][1], $nsTokens[$tokens[$i][0]])) {
+                            $namespace .= $tokens[$i][1];
+                        }
+                    } elseif ($class === true) {
+                        return "$namespace\\$token[1]";
+                    }
+
+                    break;
+            }
         }
 
         return null;
